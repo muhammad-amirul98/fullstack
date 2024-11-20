4033428e849b80a4b46a791984dc92ad2aa8efb7
muhammad-amirul98 <moamirul1998@gmail.com>
2024-11-20 12:26:00 +0800

    first commit

--- project/src/main/java/com/personal/project/ProjectApplication.java
+++ project/src/main/java/com/personal/project/ProjectApplication.java
@@ -2,16 +2,30 @@ package com.personal.project;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProjectApplication {
-	private static final Logger logger = LoggerFactory.getLogger(ProjectApplication.class);
+import com.personal.project.domain.CarRepository;
 
+@SpringBootApplication
+public class ProjectApplication implements CommandLineRunner {
+	private static final Logger logger = LoggerFactory.getLogger(ProjectApplication.class);
+	
+	private final CarRepository repository;
+	
+	public ProjectApplication(CarRepository repository) {
+		this.repository = repository;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 		logger.info("Application started");
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		//code
 	}
 
 }
